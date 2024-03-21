919ef07b533bf5eb13858ec6fcf7024d5615bdb5
sandra.greenhalgh <sandra.greenhalgh@vedi.io>
2024-03-21 22:35:12 +0800

    ci: setup github actions

---
+++ test/SaanSoft.Tests.Cqrs.Core/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SaanSoft.Tests.Cqrs.Core;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
