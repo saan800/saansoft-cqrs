7b869b1d843ecb114306c4867bcc250d29398717
sandra.greenhalgh <sandra.greenhalgh@vedi.io>
2024-03-25 22:08:13 +0800

    test dotnet format

--- src/SaanSoft.Cqrs.Core/Class1.cs
+++ src/SaanSoft.Cqrs.Core/Class1.cs
@@ -19,6 +19,7 @@ public class Class1
 
     }
 
+
     public class TestClass
     {
         public required string Name { get; set; }
--- test/SaanSoft.Tests.Cqrs.Core/UnitTest1.cs
+++ test/SaanSoft.Tests.Cqrs.Core/UnitTest1.cs
@@ -14,6 +14,7 @@ public class Tests
     }
 
 
+
     // [Test]
     // public void Failing()
     // {
