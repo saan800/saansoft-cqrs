ee3bc8bc2c485a9e5e6e8987afeaf28d6b8bd802
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2024-06-26 23:28:14 +0800

    chore: renaming

--- test/SaanSoft.Tests.Cqrs.Decorator/Store/StoreCommandHandlerDecoratorTests.cs
+++ test/SaanSoft.Tests.Cqrs.Decorator/Store/StoreCommandHandlerDecoratorTests.cs
@@ -60,5 +60,55 @@ public class StoreCommandHandlerDecoratorTests : CommandSubscriptionBusDecorator
             A.CallTo(() => _repository.UpsertHandlerAsync(A<NoHandlerCommand>._, A<Type>._, A<Exception>.That.IsNotNull(), A<CancellationToken>._)).MustHaveHappenedOnceExactly();
         }
     }
+
+    public class RunAsyncWithResponse : StoreCommandHandlerDecoratorTests
+    {
+        [Fact]
+        public async Task Should_store_single_handler_details()
+        {
+            await SutSubscriptionBusDecorator.RunAsync(new MyCommandWithResponse { Message = "hello" });
+
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<MyCommandWithResponse>._, typeof(CommandHandler), null, A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<MyCommandWithResponse>._, A<Type>._, A<Exception>.That.IsNotNull(), A<CancellationToken>._)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task Should_not_store_zero_handler_details()
+        {
+            await SutSubscriptionBusDecorator.Invoking(y => y.RunAsync(new NoHandlerCommandWithResponse { Message = "hello" }))
+                .Should().ThrowAsync<InvalidOperationException>()
+                .Where(x => x.Message.Contains("No handler for type"));
+
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<NoHandlerCommandWithResponse>._, A<Type>._, null, A<CancellationToken>._)).MustNotHaveHappened();
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<NoHandlerCommandWithResponse>._, A<Type>._, A<Exception>.That.IsNotNull(), A<CancellationToken>._)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task Should_not_store_multiple_handlers_details()
+        {
+            var extraHandler = A.Fake<ICommandHandler<MyCommandWithResponse, string>>();
+            ServiceCollection.AddScoped<ICommandHandler<MyCommandWithResponse, string>>(_ => extraHandler);
+
+            await SutSubscriptionBusDecorator.Invoking(y => y.RunAsync(new MyCommandWithResponse { Message = "hello" }))
+                .Should().ThrowAsync<InvalidOperationException>()
+                .Where(x => x.Message.Contains("Only one handler for type"));
+
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<MyCommandWithResponse>._, A<Type>._, null, A<CancellationToken>._)).MustNotHaveHappened();
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<MyCommandWithResponse>._, A<Type>._, A<Exception>.That.IsNotNull(), A<CancellationToken>._)).MustNotHaveHappened();
+        }
+
+        [Fact]
+        public async Task Should_store_handlers_details_when_next_throws_exception()
+        {
+            AddCommandHandlerException<NoHandlerCommandWithResponse, string>();
+
+            await SutSubscriptionBusDecorator.Invoking(y => y.RunAsync(new NoHandlerCommandWithResponse { Message = "hello" }))
+                .Should().ThrowAsync<Exception>()
+                .Where(x => x.Message.Contains("it went wrong"));
+
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<NoHandlerCommandWithResponse>._, A<Type>._, null, A<CancellationToken>._)).MustNotHaveHappened();
+            A.CallTo(() => _repository.UpsertHandlerAsync(A<NoHandlerCommandWithResponse>._, A<Type>._, A<Exception>.That.IsNotNull(), A<CancellationToken>._)).MustHaveHappenedOnceExactly();
+        }
+    }
 }
 
