8964c2ee9b128944c23ec9a913485fb5b27e5c58
Sandra Greenhalgh <sandragreenhalgh@gmail.com>
2024-07-01 19:05:20 +0800

    chore: add test message assembly

--- test/SaanSoft.Tests.Cqrs.Decorator.Store.MongoDB/TestSetup.cs
+++ test/SaanSoft.Tests.Cqrs.Decorator.Store.MongoDB/TestSetup.cs
@@ -9,18 +9,18 @@ public class TestSetup : SaanSoft.Tests.Cqrs.Common.TestSetup
     protected TestSetup() : base()
     {
         var assembly = typeof(TestSetup).Assembly;
-        var name = assembly.GetName().FullName;
+        var name = assembly.GetName().Name?.Replace(".", "");
         Lazy<IMongoClient> mongoClient = new(() => new MongoClient(_temporaryMongoDb.Value.ConnectionString));
         _database = new Lazy<IMongoDatabase>(() => mongoClient.Value.GetDatabase(name));
         MongoDbConfiguration.Setup(new MongoDbConfigurationOptions
         {
             ConfigureGuidId = true,
             CamelCaseElementName = true,
-            RegisterMessageClassMapForAssemblies = new List<Assembly>
-            {
+            RegisterMessageClassMapForAssemblies =
+            [
                 assembly,
                 typeof(MyCommand).Assembly
-            }
+            ]
         });
     }
 
