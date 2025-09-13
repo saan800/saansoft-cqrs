26421330044a6be8775ef63aede75e06d8bbb466
saan800 <sandragreenhalgh@gmail.com>
2025-09-13 20:43:42 +0800

    chore: skelton github actions

--- tests/SaanSoft.Tests.Cqrs.Shared/FakeTest.cs
+++ tests/SaanSoft.Tests.Cqrs.Shared/FakeTest.cs
@@ -3,7 +3,7 @@ namespace SaanSoft.Tests.Cqrs.Shared;
 public class FakeTest
 {
     /// <summary>
-    /// Keeps .Net happy to have one test in the project
+    /// Keeps dotnet happy to have one test in the project
     /// </summary>
     [Fact]
     public void ATest()
